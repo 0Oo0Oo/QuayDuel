768147db8a7d841dac18dcd7c70101308907e7d6
Medow3 <matthew.dawsonhome@gmail.com>
2020-02-29 20:38:44 -0800

    -adding spell base.gd, area2d.gd and temp_fireball.png to path

--- Universe.gd
+++ Universe.gd
@@ -11,6 +11,3 @@ func _process(delta):
 	timer += 1
 
 
-# this function is called when the spell collides with the player
-func _on_Area2D_body_entered(body: PhysicsBody2D) -> void:
-	body.damaged(10)
