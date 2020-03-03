8d0cd92d29acc32d57c41d37d9b2d939031f81c9
Medow3 <matthew.dawsonhome@gmail.com>
2020-03-02 21:30:31 -0800

    fireball gets cast on first movement of player

--- Scripts/Dodge/spell_script.gd
+++ Scripts/Dodge/spell_script.gd
@@ -11,31 +11,31 @@ var damage = 10
 
 var movement = Vector2()
 var player_position = Vector2()
+var player_movement = Vector2()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	movement = Vector2(5, 5)
+	emit_signal("fireball_collision_with_wall")
+	movement = velocity * 1/60 * player_movement.normalized()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var collision = move_and_collide(movement)
 	
-	
 	if collision != null:
 		emit_signal("fireball_collision_with_wall")
 		movement = player_position - position
 		movement = velocity * delta * movement.normalized()
-	
-# makes movement for changing direction after hitting a wall and _ready()
-func make_movement(delta):
-	emit_signal("fireball_collision_with_wall")
-	movement = player_position - position
-	movement = velocity * delta * movement.normalized()
+
+
 
 # this function is called when the spell collides with the player
 func _on_Area2D_body_entered(body: PhysicsBody2D) -> void:
 	body.damaged(damage)
 
-func _on_Player2_give_position(player_pos) -> void:
+
+func _on_Player2_give_player_info(player_pos, player_move) -> void:
 	player_position = player_pos
+	player_movement = player_move
