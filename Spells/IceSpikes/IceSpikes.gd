8df8b533009e3bb5acaf8bb3b265c52973a874a2
marcusdunn <marcus.s.dunn@gmail.com>
2020-03-15 10:40:09 -0700

    added IceSpike related untracked files

---
+++ Spells/IceSpikes/IceSpikes.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func init(player):
+	self.player = player
+
+
+func _on_Create_New_Timer_timeout():
+	print("new spike")
+	var spike = load("res://Spells/IceSpikes/IceSpike.tscn").instance()
+	spike.position = player.position
+	self.add_child(spike)
+
+
+func _on_End_Spell_Timer_timeout():
+	self.queue_free()
